1eaec07afcd423dbec6c8f79410e7ee018351e76
ScruffyFurn <scruffyfurn@hotmail.com>
2013-11-14 15:18:28 -0500

    Code commit
    Make sure you build before running, players have been deleted to save
    space (100MB per file limit)

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController SP;
+	GameObject Player, AI;
+	PlayerController playerController;
+	AIController aiController;
+	GUIText guiText;
+
+	enum GameState
+	{
+		Start,
+		Playing,
+		Won,
+		Lost,
+	};
+
+	GameState CurrentState = GameState.Start;
+
+	// Use this for initialization
+	void Start () {
+		SP = this;
+		Player = GameObject.FindWithTag("Player");
+		AI = GameObject.FindWithTag("AI");
+
+		guiText = GameObject.Find("GUI Text").GetComponent<GUIText>();
+
+		playerController = Player.GetComponent<PlayerController>();
+		aiController = AI.GetComponent<AIController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		switch(CurrentState)
+		{
+		case GameState.Start:
+
+			guiText.text = "Click, Tap or Press a key to Start";
+
+			if(Input.anyKey)
+			{
+				guiText.enabled = false;
+				playerController.Active = true;
+				aiController.Active = true;
+				CurrentState = GameState.Playing;
+			}
+			break;
+		case GameState.Playing:
+			if(playerController.OutOfBounds)
+			{
+				CurrentState = GameState.Lost;
+			}
+
+			if(aiController.OutOfBounds)
+			{
+				CurrentState = GameState.Won;
+			}
+			break;
+		case GameState.Won:
+
+				guiText.enabled = true;
+				guiText.text = "You Win!";
+			if(Input.anyKey)
+			{
+				Reset();
+			}
+			break;
+		case GameState.Lost:
+
+				guiText.enabled = true;
+				guiText.text = "You Lost!";
+			if(Input.anyKey)
+			{
+				Reset();
+			}
+			break;
+		
+		}
+	}
+
+	public void paused()
+	{
+		Time.timeScale = 0.0f;
+		guiText.enabled = true;
+		guiText.text = "Paused!";
+	}
+
+	public void unpaused()
+	{
+		Time.timeScale = 1.0f;
+		guiText.enabled = false;
+	}
+
+	void Reset()
+	{
+		playerController.OutOfBounds = false;
+		aiController.OutOfBounds = false;
+		Player.transform.rotation = new Quaternion(0,0,0,0);
+		Player.transform.position = new Vector3(0.00375f,0.3530463f,-0.9551017f);
+		playerController.Active = false;
+		AI.transform.rotation = new Quaternion(0,0,0,0);
+		AI.transform.position = new Vector3(0.00375f,0.3449954f,0.9198211f);
+		aiController.Active = false;
+		CurrentState = GameState.Start;
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+
+}
---
+++ Windows Store Build/SumoBlocks/MainPage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Template
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private SplashScreen splash;
+        private Rect splashImageRect;
+
+        //Place holder for our default screeen size
+        private double defaultWidth = Window.Current.Bounds.Width;
+        private double defaultHeight = Window.Current.Bounds.Height;
+        
+
+        public MainPage(SplashScreen splashScreen)
+        {
+            this.InitializeComponent();
+
+            splash = splashScreen;
+            OnResize();
+            Window.Current.SizeChanged += new WindowSizeChangedEventHandler((o, e) => OnResize());
+
+        }
+
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            splash = (SplashScreen)e.Parameter;
+            OnResize();
+        }
+
+        private void OnResize()
+        {
+            if (splash != null)
+            {
+                splashImageRect = splash.ImageLocation;
+                PositionImage();
+            }
+
+            /// <summary>
+            /// Added to handle screen size change. Pauses the game
+            /// </summary>
+            /// This is a simple example of how to handle screen resize
+            /// for our game. If the game window is half size or less we pause 
+            if(GameController.SP != null)
+            { 
+                if(Window.Current.Bounds.Width <= defaultWidth /2)
+                {
+                    GameController.SP.paused();
+                }
+                else if (Window.Current.Bounds.Width > defaultWidth / 2)
+                {
+                    GameController.SP.unpaused();
+                }
+            }
+        }
+
+        private void PositionImage()
+        {
+            ExtendedSplashImage.SetValue(Canvas.LeftProperty, splashImageRect.X);
+            ExtendedSplashImage.SetValue(Canvas.TopProperty, splashImageRect.Y);
+            ExtendedSplashImage.Height = splashImageRect.Height;
+            ExtendedSplashImage.Width = splashImageRect.Width;
+        }
+
+		public SwapChainBackgroundPanel GetSwapChainBackgroundPanel()
+		{
+			return DXSwapChainPanel;
+		}
+
+        public void RemoveSplashScreen()
+        {
+            DXSwapChainPanel.Children.Remove(ExtendedSplashImage);
+        }
+    }
+}
