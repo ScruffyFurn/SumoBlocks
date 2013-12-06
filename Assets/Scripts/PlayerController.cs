84447584bb91b42cbc28a27393c451bc46011420
ScruffyFurn <scruffyfurn@hotmail.com>
2013-12-06 08:35:42 -0800

    Clean up for some github repo issues

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+	public int PlayerSpeed = 10;
+	private float MoveAmmount = 0.0f;
+	private bool active = false;
+	private bool isOutOfBounds = false;
+
+	//Getters and Setters
+	public bool Active
+	{
+		get
+		{
+			return active;
+		}
+		set
+		{
+			active = value;
+		}
+	}
+	
+	public bool OutOfBounds
+	{
+		get
+		{
+			return isOutOfBounds;
+		}
+		set
+		{
+			isOutOfBounds = value;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(active)
+		{
+			if(Application.platform == RuntimePlatform.WP8Player)
+			{
+				MoveAmmount = (PlayerSpeed  * Input.acceleration.x )* Time.deltaTime;
+				transform.Translate(Vector3.right * MoveAmmount);
+				MoveAmmount = (PlayerSpeed * 0.5f * ((-Input.acceleration.z + 0.5f)*2)) * Time.deltaTime;
+				transform.Translate(Vector3.forward * MoveAmmount);
+			}
+			else
+			{
+				MoveAmmount = (PlayerSpeed *Input.GetAxis("Horizontal")) * Time.deltaTime;
+				transform.Translate(Vector3.right * MoveAmmount);
+				MoveAmmount = (PlayerSpeed *Input.GetAxis("Vertical")) * Time.deltaTime;
+				transform.Translate(Vector3.forward * MoveAmmount);
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if(collision.collider.gameObject.tag == "OutOfRing")
+		{
+			isOutOfBounds = true;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+SumoBlocks
+==========
+
+Demo game created to show how to port Unity games to Windows Phone and Windows 8.1
+
+You must build before running, players have been deleted to save space
